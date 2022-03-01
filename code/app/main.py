0d00c8674c84976533194a55bc2425055d5f4782
root <root@DESKTOP-JP.localdomain>
2022-03-01 13:46:22 +0100

    Added speech to speech

--- code/app/main.py
+++ code/app/main.py
@@ -94,7 +94,9 @@ class App:
                                    "Entity Extraction": "entity_extraction",
                                    "Entity Linking": "entity_linking",
                                    "Text Translation": "text_translation",
-                                   "Speech to Text": "speech_to_text"},
+                                   "Speech to Text": "speech_to_text",
+                                   "Text to Speech": "text_to_speech",
+                                   "Speech to Speech Translation": "speech_to_speech"},
                       "Computer Vision": {"Object Classification": "object_classification", 
                                           "Object Detection": "object_detection" }}
 
@@ -341,6 +343,82 @@ class App:
             st.markdown(wavpath)
             self.display_wavfile(wavpath)
             self.plot_wav(wavpath)
+
+    def text_to_speech(self, demo):
+        st.write(demo)
+        file_name = "outputaudio.wav"
+        file_config = speech_sdk.audio.AudioOutputConfig(filename=file_name)
+        voice = "en-GB-LibbyNeural"  #"en-GB-RyanNeural"
+        speech_config = speech_sdk.SpeechConfig(st.session_state.cog_key, st.session_state.cog_region)
+        speech_config.speech_synthesis_voice_name = voice
+        speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config, audio_config=file_config)
+        input_text = "Sugerlump was a rocking horse"
+        speak = speech_synthesizer.speak_text_async(input_text).get()
+       
+        audio_file = open(file_name, 'rb')
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes, format='audio/ogg')
+
+    
+    def speech_to_speech(self, demo):
+        st.write(demo)
+
+        st.markdown('# recorder')
+    
+        if "wavpath" not in st.session_state:
+            cur_time = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+            tmp_wavpath = TMP_DIR / f'{cur_time}.wav'
+            st.session_state["wavpath"] = str(tmp_wavpath)
+
+        if "wavpath2" not in st.session_state:
+            cur_time = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+            tmp_wavpath = TMP_DIR / f'{cur_time}.wav'
+            st.session_state["wavpath2"] = str(tmp_wavpath)
+
+        wavpath = st.session_state["wavpath"]
+        wavpath2 = st.session_state["wavpath2"]
+
+        self.aiortc_audio_recorder(wavpath)  # first way
+        # save_frames_from_audio_receiver(wavpath)  # second way
+
+        if Path(wavpath).exists():
+            speech_config = speech_sdk.SpeechConfig(st.session_state.cog_key, st.session_state.cog_region)
+            print('Ready to use speech service in:', speech_config.region)
+            audio_config = speech_sdk.AudioConfig(filename=wavpath)
+            speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+            speech = speech_recognizer.recognize_once_async().get()
+            command = speech.text
+            print(command)
+            st.write(command)
+            st.markdown(wavpath)
+            self.display_wavfile(wavpath)
+            #self.plot_wav(wavpath)
+            # Configure translation
+        
+            translation_config = speech_sdk.translation.SpeechTranslationConfig(st.session_state.cog_key, st.session_state.cog_region)
+            translation_config.speech_recognition_language = 'en-GB'
+            translation_config.add_target_language('fr')
+            print('Ready to translate from',translation_config.speech_recognition_language)
+            
+            audio_config = speech_sdk.AudioConfig(filename=wavpath2)
+            translator = speech_sdk.translation.TranslationRecognizer(translation_config, audio_config)
+            print("Getting speech from file...")
+            result = translator.recognize_once_async().get()
+            print('Translating "{}"'.format(result.text))
+            translation = result.translations['fr']
+            print(translation)
+
+            file_config = speech_sdk.audio.AudioOutputConfig(filename=wavpath2)
+            speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config, audio_config=file_config)
+            speak = speech_synthesizer.speak_text_async(translation).get()
+       
+            audio_file = open(wavpath2, 'rb')
+            audio_bytes = audio_file.read()
+            st.audio(audio_bytes, format='audio/ogg')
+    
+
+
+
 
 if __name__ == "__main__":
     app = App()
